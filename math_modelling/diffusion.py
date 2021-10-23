bff1144616660bb02d9ddc72b703c801bdad33ae
ZexUlt <danil.chernozubov66@gmail.com>
2021-10-24 00:57:01 +0300

    WIP

--- math_modelling/diffusion.py
+++ math_modelling/diffusion.py
@@ -1,9 +1,10 @@
 from typing import Callable
 import numpy as np
 
+from exceptions import FunctionNotInitialized
 
 class DiffusionSolver:
-    def __init__(self, N: int, T: int, tau: float, D: float):
+    def __init__(self, N: int, T: int, tau: float, D: float) -> None:
         self.D = D
         self.N = N
         self.h = 2*np.pi / N
@@ -14,24 +15,29 @@ class DiffusionSolver:
         self.eigenvals = np.array(
             [4*self.D / self.h**2 * np.sin(k*self.h/4) for k in range(N)]
             )
+        self.__reconstruct_eigenvals_array__()
     
     def __reconstruct_eigenvals_array__(self):
         reversed_half = self.X[self.N/2:]
         reversed_half = reversed_half[::-1]
         self.eigenvals = np.append(reversed_half, self.eigenvals[:self.N/2])
-    
-    def __calculate_F_Fourier_coeff__(self):
-        self.F = np.fft.fft()
+
+    def run(self) -> None:
+        if not self.U0:
+            raise FunctionNotInitialized(f'Function U0 should be set before running solver.')
+        
+        u = self.U0(self.X)
+        Fu = np.fft.fft(u)
 
 
-    def set_F_function(self, F: Callable):
+    def set_F_function(self, F: Callable) -> None:
         if callable(F):
-            self.F = F
+            self.F = np.vectorize(F)
         else:
-            raise TypeError(f"Argument should be callable. Got {type(F)}")
+            raise TypeError(f"Argument should be callable. Got {type(F)}.")
     
-    def set_U0_function(self, U0: Callable):
+    def set_U0_function(self, U0: Callable) -> None:
         if callable(U0):
-            self.U0 = U0
+            self.U0 = np.vectorize(U0)
         else: 
-            raise TypeError(f"Argument should be callable. Got {type(U0)}.\n")
+            raise TypeError(f"Argument should be callable. Got {type(U0)}.")
